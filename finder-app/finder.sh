34a6954f0eab09bd9dfd58ad678ef6e97c573a43
EsGeh <samuel@esgeh.net>
2025-06-02 00:10:15 +0200

    implement assignment3 part2

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 print_usage() {
 	echo "usage: $0 DIR SEARCH_STR"
 }
 
 
-if [[ "$#" -ne 2 ]]; then
+if [ "$#" -ne 2 ]; then
 	print_usage
 	exit 1
 fi
@@ -13,15 +13,14 @@ fi
 filesdir=$1
 searchstr=$2
 
-if [[ ! -d "$filesdir" ]]; then
+if [ ! -d "$filesdir" ]; then
 	echo "ERROR: '$filesdir' is not a directory" >&2
 	exit 1
 fi
 
 finder() {
-	res=( $(grep --recursive --count --no-filename "$2" "$1") )
-	file_count=${#res[@]}
-	result_count=$(awk '{ s+=$1 } END { print s}' <( for line in "${res[@]}"; do echo "$line"; done ))
+	file_count=$(grep -r -c "$2" "$1" | wc -l)
+	result_count=$(grep -r "$2" "$1" | wc -l)
 	echo "The number of files are $file_count and the number of matching lines are $result_count"
 }
 
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -21,7 +21,11 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+mkdir --parents "${OUTDIR}"
+if [ $? -ne 0 ]; then
+	echo "ERROR: failed creating directory '$OUTDIR'" >&2
+	exit 1
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -34,20 +38,28 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Add your kernel build steps here
+		cp "$FINDER_APP_DIR/conf/.config" ./
+		make -j4 "CROSS_COMPILE=$CROSS_COMPILE" "ARCH=$ARCH" all
 fi
 
 echo "Adding the Image in outdir"
+cp "$OUTDIR/linux-stable/arch/arm64/boot/Image" "$OUTDIR/"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir "$OUTDIR/rootfs"
+cd "$OUTDIR/rootfs"
+mkdir --parents bin dev etc home lib lib64 proc sys sbin tmp usr var
+mkdir --parents usr/bin usr/lib usr/sbin
+mkdir --parents var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +67,51 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+		make distclean
+		make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+make -j4 "CROSS_COMPILE=$CROSS_COMPILE" "ARCH=$ARCH"
+make -j4 "CROSS_COMPILE=$CROSS_COMPILE" "ARCH=$ARCH" "CONFIG_PREFIX=$OUTDIR/rootfs" install
+
+cd "$OUTDIR/rootfs"
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+TOOLCHAIN_FS=$(dirname $(which "${CROSS_COMPILE}readelf"))/..
+cp "$TOOLCHAIN_FS/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1" "$OUTDIR/rootfs/lib/"
+cp "$TOOLCHAIN_FS/aarch64-none-linux-gnu/libc/lib64/libm.so.6" "$OUTDIR/rootfs/lib64/"
+cp "$TOOLCHAIN_FS/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2" "$OUTDIR/rootfs/lib64/"
+cp "$TOOLCHAIN_FS/aarch64-none-linux-gnu/libc/lib64/libc.so.6" "$OUTDIR/rootfs/lib64/"
 
-# TODO: Make device nodes
+# Make device nodes
+cd "$OUTDIR/rootfs"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+make "CROSS_COMPILE=$CROSS_COMPILE" clean all
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer "$OUTDIR/rootfs/home/"
+cp finder.sh "$OUTDIR/rootfs/home/"
+cp -rL conf "$OUTDIR/rootfs/home/"
+cp finder-test.sh "$OUTDIR/rootfs/home/"
+cp autorun-qemu.sh "$OUTDIR/rootfs/home/"
 
-# TODO: Chown the root directory
+# Chown the root directory
+sudo chown --recursive root:root "$OUTDIR/rootfs"
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+gzip -f "$OUTDIR/initramfs.cpio"
