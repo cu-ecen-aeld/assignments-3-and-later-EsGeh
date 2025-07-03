eb0db2b1563fc0009f7ae751516cf12361b3b8f7
Samuel Gfrörer <samuel@esgeh.net>
2025-07-03 13:51:54 +0200

    implement assignment 7-1 (ring buffer).

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -10,29 +10,41 @@
 
 #ifdef __KERNEL__
 #include <linux/string.h>
+#define DEBUG_LOG(fmt,...)
 #else
 #include <string.h>
+#include <stdio.h>
+#define DEBUG_LOG(fmt,...)
+// #define DEBUG_LOG(fmt,...) fprintf( stderr, fmt, ## __VA_ARGS__ )
 #endif
 
 #include "aesd-circular-buffer.h"
 
 /**
- * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
- * @param char_offset the position to search for in the buffer list, describing the zero referenced
- *      character index if all buffer strings were concatenated end to end
- * @param entry_offset_byte_rtn is a pointer specifying a location to store the byte of the returned aesd_buffer_entry
- *      buffptr member corresponding to char_offset.  This value is only set when a matching char_offset is found
- *      in aesd_buffer.
- * @return the struct aesd_buffer_entry structure representing the position described by char_offset, or
- * NULL if this position is not available in the buffer (not enough data is written).
+ * @param buffer the buffer to search for corresponding offset. Any necessary locking must be performed by caller.
+ * @param char_offset the position to search for in the buffer list, describing the zero referenced character index if all buffer strings were concatenated end to end
+ * @param entry_offset_byte_rtn is a pointer specifying a location to store the byte of the returned aesd_buffer_entry buffptr member corresponding to char_offset.  This value is only set when a matching char_offset is found in aesd_buffer.
+ * @return the struct aesd_buffer_entry structure representing the position described by char_offset, or NULL if this position is not available in the buffer (not enough data is written).
  */
-struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
-            size_t char_offset, size_t *entry_offset_byte_rtn )
+struct aesd_buffer_entry* aesd_circular_buffer_find_entry_offset_for_fpos(
+		struct aesd_circular_buffer* buffer,
+    size_t char_offset,
+		size_t* entry_offset_byte_rtn
+)
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+	size_t pos_bytes = 0;
+	unsigned int pos = buffer->out_offs;
+	do
+	{
+		if( char_offset < pos_bytes + buffer->entry[pos].size ) {
+			(*entry_offset_byte_rtn) = char_offset - pos_bytes;
+			return &buffer->entry[pos];
+		}
+		pos_bytes += buffer->entry[pos].size;
+		pos = (pos + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	}
+	while( pos != buffer->out_offs );
+	return NULL;
 }
 
 /**
@@ -42,11 +54,38 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * Any necessary locking must be handled by the caller
 * Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
 */
-void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+void aesd_circular_buffer_add_entry(
+		struct aesd_circular_buffer* buffer,
+		const struct aesd_buffer_entry* add_entry
+)
 {
-    /**
-    * TODO: implement per description
-    */
+	// 1. determine number of entries:
+	unsigned int entry_count;
+	if( !buffer->full ) {
+		if( buffer->out_offs <= buffer->in_offs) {
+			entry_count = buffer->in_offs - buffer->out_offs;
+		}
+		else {
+			entry_count = (buffer->in_offs + AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) - buffer->out_offs;
+		}
+	}
+	else {
+		// assert( buffer->out_offs == buffer->in_offs );
+		entry_count = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	}
+	DEBUG_LOG( "count: %d\n", entry_count );
+	// 2. add element:
+	buffer->entry[buffer->in_offs] = (*add_entry);
+	buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	// 3. handle corner cases:
+	if( entry_count < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED ) {
+		if( entry_count+1 == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED ) {
+			buffer->full = true;
+		}
+	}
+	else {
+		buffer->out_offs = buffer->in_offs;
+	}
 }
 
 /**
